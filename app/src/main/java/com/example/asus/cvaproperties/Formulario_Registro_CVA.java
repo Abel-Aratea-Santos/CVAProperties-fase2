152aba0fef6e1fc6d43be1c8faa80205f36b2f75
CVAProperties <cvaofproperties@gmail.com>
2018-07-07 17:19:07 -0400

    fase 5

--- app/src/main/java/com/example/asus/cvaproperties/Formulario_Registro_CVA.java
+++ app/src/main/java/com/example/asus/cvaproperties/Formulario_Registro_CVA.java
@@ -34,16 +34,7 @@ public class Formulario_Registro_CVA extends AppCompatActivity implements View.O
 
         LoadComponents();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
 
-
-            }
-        });
     }
 
     private void LoadComponents() {
