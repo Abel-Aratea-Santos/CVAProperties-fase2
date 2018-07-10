bc6b28c391160eba9d0c4ddbde86c3d14495cea8
Erik_Jonathan_Perez_Flores <Erik.jonathan.perez.flores@gmail.com>
2018-07-10 03:23:23 -0400

    Lat_Lon_Mapsv2

--- app/src/main/java/com/example/asus/cvaproperties/LatLon_Mapas.java
+++ app/src/main/java/com/example/asus/cvaproperties/LatLon_Mapas.java
@@ -2,19 +2,18 @@ package com.example.asus.cvaproperties;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
-import com.example.asus.cvaproperties.DATA.DataApp;
-import com.example.asus.cvaproperties.DATA.UserData;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -26,7 +25,7 @@ import org.json.JSONArray;
 
 import cz.msebera.android.httpclient.Header;
 
-public class LatLon_Maps extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+public class LatLon_Mapas extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
     private GoogleMap mMap;
     private MarkerOptions marker;
     private Context root;
@@ -59,7 +58,7 @@ public class LatLon_Maps extends FragmentActivity implements OnMapReadyCallback,
                     RequestParams params= new RequestParams();
                     params.put("lat_a",marker.getPosition().latitude);
                     params.put("lon_a",marker.getPosition().longitude);
-                    client.patch(DataApp.REST_HOME_PATCH + UserData.ID,params,new JsonHttpResponseHandler(){
+                    client.patch("http://192.168.1.6:5000/api/v1.0/mapa/5b4418486d92f4342708803e",params,new JsonHttpResponseHandler(){
 
                         @Override
                         public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
